b50a76f81051f62697760864d5cde2e4701bb7af
IsraelMarqs <israelmarques007@gmail.com>
2025-10-16 01:29:32 -0300

    Testando autenticacao

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            model.addAttribute("userName", principal.getAttribute("name"));
+            model.addAttribute("userEmail", principal.getAttribute("email"));
+        }
+        return "index";
+    }
+
+    @GetMapping("/secure")
+    public String secure(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            model.addAttribute("userName", principal.getAttribute("name"));
+            model.addAttribute("userEmail", principal.getAttribute("email"));
+            model.addAttribute("picture", principal.getAttribute("picture"));
+        }
+        return "secure";
+    }
+}
+
